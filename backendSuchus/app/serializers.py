554a223fdbe940d548d3647575da3b453e279511
HerrThoMii <thomisz15@gmail.com>
2025-09-08 15:14:35 -0300

    Login endpoint

--- backendSuchus/app/serializers.py
+++ backendSuchus/app/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Usuario, UsuarioTipo
 
-class UsuarioRegister(serializers.ModelSerializer):
+class UsuarioRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
         fields = ["email", "contraseña", "nombre", "apellido", "telefono"]
@@ -12,3 +12,7 @@ class UsuarioRegister(serializers.ModelSerializer):
         cliente_tipo = UsuarioTipo.objects.get(descripcion="Cliente")
         validated_data['usuarioTipo'] = cliente_tipo #Chavales esto es para que sea cliente si o si
         return super().create(validated_data)
+    
+class UsuarioLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    contraseña = serializers.CharField()
--- backendSuchus/app/urls.py
+++ backendSuchus/app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UsuarioRegisterView
+from .views import UsuarioRegisterView, UsuarioLoginView
 
 urlpatterns = [
     path("register/", UsuarioRegisterView.as_view(), name='usuario-register'),
+    path("login/", UsuarioLoginView.as_view()),
 ]
