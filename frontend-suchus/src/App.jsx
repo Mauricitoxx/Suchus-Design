021284a1abf5c5f958493273ef0842f441c841da
ValentinGarzaniti <valenxity@gmail.com>
2025-09-15 21:29:31 -0300

    home,login forntend para gestion cliente

--- frontend-suchus/src/App.jsx
+++ frontend-suchus/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
+import Login from "./components/Login";
+import Home from "./components/Home";
 
 function App() {
 
@@ -8,6 +10,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Home" element={<Home />} />
       </Routes>
     </Router>
   )
